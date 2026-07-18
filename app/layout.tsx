e76157d6370c6a083d680205757da9376e2206f8
devlewiso <devlewiso@gmail.com>
2026-07-17 22:57:53 -0600

    feat: add real GA4 Measurement ID G-15KQCFTZHC
    - Replace placeholder with actual GA4 tracking ID
    - Analytics will start collecting data after next deploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -97,7 +97,7 @@ export default function RootLayout({
                 {/* Google Analytics GA4 */}
                 <script
                     async
-                    src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+                    src="https://www.googletagmanager.com/gtag/js?id=G-15KQCFTZHC"
                 />
                 <script
                     dangerouslySetInnerHTML={{
@@ -105,7 +105,7 @@ export default function RootLayout({
                             window.dataLayer = window.dataLayer || [];
                             function gtag(){dataLayer.push(arguments);}
                             gtag('js', new Date());
-                            gtag('config', 'G-XXXXXXXXXX', {
+                            gtag('config', 'G-15KQCFTZHC', {
                                 page_path: window.location.pathname,
                             });
                         `,
