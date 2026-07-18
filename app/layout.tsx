bcf8b9e355acc439e6b2e51e91043bf6499cbeb8
devlewiso <devlewiso@gmail.com>
2026-07-17 22:48:40 -0600

    feat: add Google Analytics GA4 tracking
    - Add GA4 gtag to layout.tsx head
    - Placeholder ID: G-XXXXXXXXXX (replace with actual ID)
    - Tracks page views across all routes including blog posts
    
    TODO: Replace G-XXXXXXXXXX with actual GA4 Measurement ID
    from Google Analytics admin panel.

--- app/layout.tsx
+++ app/layout.tsx
@@ -94,6 +94,23 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                {/* Google Analytics GA4 */}
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+                />
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-XXXXXXXXXX', {
+                                page_path: window.location.pathname,
+                            });
+                        `,
+                    }}
+                />
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{
